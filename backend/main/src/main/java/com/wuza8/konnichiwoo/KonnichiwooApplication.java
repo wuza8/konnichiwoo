b1090a723ed4f41afcd34cd9fdbf1313261660d1
AyBiCi <rymcymcym100@gmail.com>
2023-03-22 10:30:57 +0100

    Added spring starter

---
+++ backend/main/src/main/java/com/wuza8/konnichiwoo/KonnichiwooApplication.java
@@ -0,0 +1,13 @@
+package com.wuza8.konnichiwoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonnichiwooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonnichiwooApplication.class, args);
+	}
+
+}
---
+++ backend/main/src/test/java/com/wuza8/konnichiwoo/KonnichiwooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuza8.konnichiwoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonnichiwooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
