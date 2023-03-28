167802b5c1a8a0b18644d953715081c98047efa6
wuza8 <rymcymcym100@gmail.com>
2023-03-29 00:29:29 +0200

    Added repeat page

--- backend/main/src/main/java/com/wuza8/konnichiwoo/Endpoints.java
+++ backend/main/src/main/java/com/wuza8/konnichiwoo/Endpoints.java
@@ -1,2 +1,17 @@
-package com.wuza8.konnichiwoo;public class Endpoints {
+package com.wuza8.konnichiwoo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api")
+public class Endpoints {
+
+    @GetMapping("login")
+    public String bobby(Principal principal){
+        return "Hello "+principal.getName()+"!";
+    }
 }
--- backend/main/src/main/java/com/wuza8/konnichiwoo/Templates.java
+++ backend/main/src/main/java/com/wuza8/konnichiwoo/Templates.java
@@ -1,2 +1,9 @@
-package com.wuza8.konnichiwoo;public class Templates {
+package com.wuza8.konnichiwoo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class Templates {
 }
